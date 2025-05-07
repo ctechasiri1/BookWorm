746763b12013e32a0331b36181b20010b45090df
Chiraphat Techasiri <ctechasiri@gmail.com>
2025-05-06 20:08:37 -0700

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Chiraphat Techasiri on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
